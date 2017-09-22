c866d09c842da012d70bef5d84d2a30cb9b8799d
Acidust <tasoscarface@yahoo.com>
2017-09-22 15:56:29 +0300

    Initial Commit

---
+++ Vuejs/src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="app">
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item" href="#">
+            Sidirgot
+          </a>
+        </div>
+        <span class="nav-toggle" @click="toggleNav" v-bind:class="{ 'is-active':isActive }">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="nav-right nav-menu" @click="toggleNav" v-bind:class="{ 'is-active':isActive }">
+          <router-link class="nav-item r-item" to="/">Home</router-link>
+          <router-link class="nav-item r-item" to="/">Features</router-link>
+          <router-link class="nav-item r-item" to="/">About</router-link>
+          <router-link class="nav-item r-item" to="/faq">FAQ</router-link>
+
+          <div class="nav-item" >
+            <p class="control">
+              <a class="button is-primary is-outlined" href="#">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <router-view></router-view>
+
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>This is a simple design to test Vuejs</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#">
+              <i class="fa fa-twitter"></i>
+            </a>
+            <a class="icon" href="#">
+              <i class="fa fa-facebook"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  data: function() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggleNav: function() {
+      this.isActive = !this.isActive;
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import '../node_modules/bulma/bulma.sass'
+@import 'mediaQ'
+
+.nav
+  background-color: #383838
+  a:hover
+    color: red
+
+.nav-left a
+  color: #fff
+  font-weight: bold
+
+a.r-item
+  color: #C1C1C1
+  padding: 0.5rem 1.75rem
+
+.nav-toggle span
+  background-color: #c1c1c1
+
+footer
+  background-color: $primary !important
+  color: #fff
+
+  .icon
+    color: #fff
+    margin-left: 20px
+</style>
---
+++ Vuejs/src/components/Home.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ Heading }}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4">{{ Subheading }}</h2>
+          </div>
+          <a class="button is-large is-primary" id="learn" href="#">Learn More</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns pd is-desktop">
+          <div class="column is-1 has-text-centered">
+            <i class="fa fa-cog is-primary"></i>
+          </div>
+          <div class="column is-one-third-desktop">
+            <p class="title">
+              <strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</strong>
+            </p>
+          </div>
+          <div class="column">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns pd">
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </p>
+              <p class="subtitle">- Sidirgot</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </p>
+              <p class="subtitle">- Sidirgot</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </p>
+              <p class="subtitle">- Sidirgot</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      Heading: 'This is my heading',
+      Subheading: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../mediaQ'
+
+.hero
+  background: url('../assets/codingOp.jpg')
+  background-size: cover
+  h1,h2
+    color: #fff
+  h2
+    margin: 1.5rem 0 2rem 0 !important
+  .title
+    +mobile
+      font-weight: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      font-size: 4rem
+      margin-top: 2rem
+
+.fa-cog
+  font-size: 40px
+
+#learn
+  +desktop
+    margin-bottom: 2rem
+.pd
+  +tablet
+    padding: 2em 0
+
+
+
+
+</style>
